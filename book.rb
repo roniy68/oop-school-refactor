6213dd57f7db0886c4695f78ce3886b6c4e89096
roniy68 <ahmed.11900449@lpu.in>
2023-04-20 18:23:13 +0530

    🔥 Change book class

--- book.rb
+++ book.rb
@@ -1,19 +1,15 @@
-require_relative 'rental'
-
 class Book
   attr_accessor :title, :author
-
-  attr_reader :rental
+  attr_reader :rentals
 
   def initialize(title, author)
     @title = title
-
     @author = author
-
-    @rental = []
+    @rentals = []
   end
 
-  def add_rental(date, person)
-    Rental.new(date, self, person)
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.book = self
   end
 end
