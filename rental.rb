aa1e91b9c36f3e57acbaf4b012a07f748d41c086
roniy68 <ahmed.11900449@lpu.in>
2023-04-20 17:57:53 +0530

    🔥 Upgrade Rental Class

--- rental.rb
+++ rental.rb
@@ -1,17 +1,24 @@
 class Rental
   attr_accessor :date
-
   attr_reader :book, :person
 
   def initialize(date, book, person)
     @date = date
 
-    @book = book
-
-    book.rental << self
-
     @person = person
+    @person.rentals << self
 
-    person.rental << self
+    @book = book
+    @book.rentals << self
+  end
+
+  def book=(book)
+    @book = book
+    book.rentals << self
+  end
+
+  def person=(person)
+    @person = person
+    person.rentals << self
   end
 end
